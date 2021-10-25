df236443e2a9beaa410540b5f3104921468611f7
Paulo-Moncaio <pf.avelar.72856@gmail.com>
2021-10-24 21:51:39 -0300

    upload files

---
+++ src/test/java/br/com/fiap/epictask/EpictaskApplicationTests.java
@@ -0,0 +1,18 @@
+package br.com.fiap.epictask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpictaskApplicationTests {
+
+	public static void main(String[] args) {
+
+		System.out.println("Test.. Test by Crunchify.. \nThis is a simple tutorial on how to create .war file using Maven Plugin..");
+
+		}
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
